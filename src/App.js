9a6e76bd28d7a9efa6c71f1b8108ead1726be2b2
Corley Kennard <development@corleykennard.anonaddy.com>
2020-09-23 14:03:35 +0000

    test

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ const Navbar = (props) => {
   <nav className="navbar navbar-light bg-light justify-content-between">
   <a className="navbar-brand">Navbar</a>
   <form className="form-inline">
-    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
   </form>
 </nav>
