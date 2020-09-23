bb53e9a65dca77883d1642c1500ac0ac18564a90
Corley Kennard <development@corleykennard.anonaddy.com>
2020-09-23 11:34:07 +0000

    test

--- src/App.js
+++ src/App.js
@@ -25,22 +25,39 @@ const About = () => <h1>About</h1>;
 const Contact = () => <h1>Contact</h1>;
 const Error = () => <h1> It is Not Found</h1>;
 const LogIn = () => <h1>Please login</h1>;
-
+async function postData(url = 'https://cloud-drive.corleykennard.vercel.app/api/database', data ={ func: "getFilesInParent", arg: "/Music"}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+       credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 const FileTable = () => {
   const [data, setData] = useState([]);
-
+postData().then(data => {    console.log(data); })
   const [isLoading, setIsLoading] = useState(false);
-  let { parent } = useParams();
+  let { parent } = useParams()
   let { url } = useRouteMatch();
 
   useEffect(() => {
+    const p= `\/${parent}`
+    
     fetch("https://cloud-drive.corleykennard.vercel.app/api/database/", {
       headers: {
-        Accept: "application/json",
         "Content-Type": "application/json",
       },
       method: "POST",
-      body: JSON.stringify({ func: "getFilesInParent", arg: parent }),
+      body: JSON.stringify({ func: "getFilesInParent", arg: "./ }),
     })
       .then((res) => res.json())
       .then((res) => setData(res.data))
