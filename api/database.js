ef41e9fdda5270dd8e8efdf5df18a40ea7fbca2c
Corley Kennard <development@corleykennard.anonaddy.com>
2020-09-23 09:37:08 +0000

    test

--- api/database.js
+++ api/database.js
@@ -1,9 +1,11 @@
-var faunadb =require( "faunadb")
+var faunadb = require("faunadb");
 let q = faunadb.query;
 let client = new faunadb.Client({
-  secret: "fnAD2Hj7UnACEwHOXkqYs-z8zKVUl6FuUCmXHL-j"
+  secret: "fnAD2Hj7UnACEwHOXkqYs-z8zKVUl6FuUCmXHL-j",
 });
 module.exports = async (req, res) => {
-let r = await  client.query(q.Call(q.Function('getFilesInParent''), req.body.parent))
-res.status(200).json(r)
-}
+  let r = await client.query(
+    q.Call(q.Function("getFilesInParent"), req.body.parent)
+  );
+  res.status(200).json(r);
+};
