3c7bc63bedc25c56782723faa4547e28d231995a
Corley Kennard <development@corleykennard.anonaddy.com>
2020-09-23 13:59:26 +0000

    test

--- src/App.js
+++ src/App.js
@@ -14,18 +14,7 @@ import faunadb from "faunadb";
 let q = faunadb.query;
 
 let api_url = "https://cloud-drive.vercel.app/api/";
-
-const Home = (props) => (
-  <h1>
-    {" "}
-    My Home is in {props.location},{props.country}
-  </h1>
-);
-const About = () => <h1>About</h1>;
-const Contact = () => <h1>Contact</h1>;
 const Error = () => <h1> It is Not Found</h1>;
-const LogIn = () => <h1>Please login</h1>;
-
 const FileTable = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -73,48 +62,19 @@ let {url } = useRouteMatch();
   );
 };
 
-const User = (props) => {
-  let username = props.username;
-  return (
-    <div>
-      <h1>Welcome {username}</h1>;
-    </div>
-  );
-};
-
 const Navbar = (props) => {
   return (
-    <ul>
-      <li>
-        <NavLink to="/">HOME</NavLink>
-      </li>
-      <li>
-        <NavLink to="/about">ABOUT</NavLink>
-      </li>
-      <li>
-        <NavLink to="/parent/Music">Music</NavLink>
-      </li>
-      <li>
-        <NavLink to="/user/Asabeneh">User Asabeneh</NavLink>
-      </li>
-      <li>
-        <NavLink to="/user/John">User John</NavLink>
-      </li>
-    </ul>
+  <nav className="navbar navbar-light bg-light justify-content-between">
+  <a className="navbar-brand">Navbar</a>
+  <form className="form-inline">
+    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</nav>
   );
 };
 
 class App extends Component {
-  state = {
-    loggedIn: false,
-  };
-
-  handleLogin = () => {
-    this.setState((prevState) => ({
-      loggedIn: !prevState.loggedIn,
-    }));
-  };
-
   render() {
     return (
       <div className="App">
@@ -127,12 +87,9 @@ class App extends Component {
                 exact
                 path="/"
                 render={() => {
-                  return <Redirect to="/parent/" />;
+                  return <Redirect to="/list?path=/" />;
                 }}
               />
-              <Route exact strict path="/about" component={About} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/Music" component={LogIn} />
               <Route path="/list" component={FileTable} />
               <Route component={Error} />
             </Switch>
